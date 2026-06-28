429866d0f6d83963f709e528bf9d28cb3e5e6acf
Shakib Uz Zaman <207609386+Shakib-Uz-Zaman@users.noreply.github.com>
2026-06-28 21:21:42 +0600

    tolower function add

--- vowel_consonant.cpp
+++ vowel_consonant.cpp
@@ -7,8 +7,9 @@ int main()
     char word;
     cout<<"Enter a character :";
     cin>>word;
-
-    if (word == 'a' or word == 'e' or word == 'i' or word == 'o' or word == 'u' or word == 'A' or word == 'E' or word == 'I' or word == 'O' or word == 'U')
+    word = tolower(word);
+    
+    if (word == 'a' or word == 'e' or word == 'i' or word == 'o' or word == 'u')
     {
         cout<<word<<" is a vowel.";
     }
